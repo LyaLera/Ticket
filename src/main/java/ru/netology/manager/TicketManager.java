897d9d31810513af15d20319fe9f418ed2245721
Lera Lyakisheva <leralyakisheva@gmail.com>
2022-01-26 22:03:13 +0300

    searchBy changed

--- src/main/java/ru/netology/manager/TicketManager.java
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -18,7 +18,7 @@ public class TicketManager {
     public Ticket[] searchBy(String text, String text1) {
         Ticket[] result = new Ticket[0];
         for (Ticket ticket : repository.findAll(text, text1)) {
-            if (matches(ticket, text, text1)) {
+            if (ticket.getFrom().contains(text) && ticket.getTo().contains(text1)) {
                 Ticket[] tmp = new Ticket[result.length + 1];
                 System.arraycopy(result, 0, tmp, 0, result.length);
                 tmp[tmp.length - 1] = ticket;
@@ -27,12 +27,4 @@ public class TicketManager {
         }
         return result;
     }
-
-    public boolean matches(Ticket ticket, String search, String search1) {
-        if (ticket.getFrom().contains(search) && ticket.getTo().contains(search1)) {
-            return true;
-        }
-        return false;
-    }
-
 }
